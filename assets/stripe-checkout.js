cc7500aedcf521dffba374e27915221341f6b108
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-04-21 05:34:10 -0500

    continua proceso

--- assets/stripe-checkout.js
+++ assets/stripe-checkout.js
@@ -3,8 +3,8 @@ import STRIPE_KEYS from "./stripe-keys.js";
 //console.log(stripeKeys);
 
 const d = document,
-    $products = d.getElementById("products"),
-    $template = d.getElementById("product-template").contentEditable,
+    $dibujos = d.getElementById("dibujos"),
+    $template = d.getElementById("dibujo-template").content,
     $fragment = d.createDocumentFragment(),
     fetchOptions = {
         headers: {
@@ -12,7 +12,10 @@ const d = document,
         },
     };
 
-let prices, products;
+let products, prices;
+
+// funcion para formatear el precio de los productos
+const moneyFormat = (num) => `$${num.slice(0, -2)}.${num.slice(-2)}`;
 
 // pedir mis datos usando la API ajax fetch usando mi clave secreta de la API stripe
 Promise.all([
@@ -21,11 +24,62 @@ Promise.all([
 ])
     .then((responses) => Promise.all(responses.map((res) => res.json())))
     .then((json) => {
-        console.log(json);
+        // console.log(json);
+        products = json[0].data;
+        prices = json[1].data;
+        console.log(products, prices);
+
+        // recorrer el resultado
+        prices.forEach((price) => {
+            let productData = products.filter(
+                (product) => product.id === price.product
+            );
+            console.log(productData);
+
+            $template
+                .querySelector(".dibujo")
+                .setAttribute("data-price", price.id);
+            $template.querySelector("img").src = productData[0].images[0];
+            $template.querySelector("img").alt = productData[0].name;
+            $template.querySelector("figcaption").innerHTML = `
+                ${productData[0].name}
+                <br>
+                ${price.currency} ${moneyFormat(price.unit_amount_decimal)}
+            `;
+
+            let $clone = d.importNode($template, true);
+            $fragment.appendChild($clone);
+        });
+        $dibujos.appendChild($fragment);
     })
     .catch((err) => {
         let message =
             err.statusText ||
             "Ocurrio un error al conectarse con la API Stripe";
-        $products.innerHTML = `${message}: ${err.status}`;
+        $dibujos.innerHTML = `${message}: ${err.status}`;
     });
+
+d.addEventListener("click", (event) => {
+    // si el click es la clase o cualquier cosa adentro
+    if (event.target.matches(".dibujo *")) {
+        // ir al elemento padre y obtener el data-price creado anteriormente
+        let priceId = event.target.parentElement.getAttribute("data-price");
+        // enviar el precio a stripe usando llave publica
+        Stripe(STRIPE_KEYS.public)
+            .redirectToCheckout({
+                lineItems: [{ price: priceId, quantity: 1 }],
+                mode: "subscription",
+                // pagina de exito en local
+                successUrl: "http://127.0.0.1:5500/stripe-success.html",
+                // pagina de error
+                cancelUrl: "",
+            })
+            // manipular el error
+            .then((res) => {
+                if (res.error) {
+                    // insertar mensaje debajo de la imagen
+                    $dibujos.insertAdjacentHTML("afterend", res.error.message);
+                }
+            });
+    }
+});
